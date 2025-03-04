df409f955c87740b2a72460e256c229fc4f025a1
Michaela Očkovská <michaela.ockovska@hptronic.cz>
2025-03-04 08:46:07 +0100

    Add paragraphs and DOM manipulation examples in HTML and JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 /** Document Object Model = DOM */
+
+// querySelector()
+const heading = document.querySelector('h1')
+console.log(heading)
+
+const paragraph = document.querySelector('p')
+console.log(paragraph)
+
+// querySelectorAll()
+const allParagraphs = document.querySelectorAll('p')
+console.log(allParagraphs)
+console.log(allParagraphs[0])
+console.log(allParagraphs[1])
+
+// getElementById()
+const myId = document.getElementById('btn')
+console.log(myId)
+
+// getElementsByClassName()
+const myClass = document.getElementsByClassName('paragraph')
+console.log(myClass)
+console.log(myClass[0])
+console.log(myClass[1])
+
+// Finding ID and Classes by querySelector()
+const myIdQS = document.querySelector('#btn')
+const myClassQS = document.querySelector('.paragraph')
+
+console.log(`This is selector by ID`, myIdQS)
+console.log(`This is selector by class`, myClassQS)

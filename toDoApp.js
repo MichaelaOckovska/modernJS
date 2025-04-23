bed74abd2f0d576f36927e9623d06ddb64e41b88
Michaela Očkovská <michaela.ockovska@hptronic.cz>
2025-04-23 06:16:53 +0200

    JS76 - finished

--- toDoApp.js
+++ toDoApp.js
@@ -4,4 +4,7 @@ document.querySelector('a').addEventListener('click', (event) => {
   console.log(`He is alive`)
   console.log(event)
   console.log(event.altKey)
+  console.log(event.target)
+  console.log(event.target.textContent)
+  event.target.textContent = 'It was clicked'
 })

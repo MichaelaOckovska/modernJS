6d6c1920bd5054f50a22c101318f61b55a16277a
Michaela Očkovská <michaela.ockovska@hptronic.cz>
2025-04-23 06:38:25 +0200

    JS77 - finished

--- To Do App/00_repetition.js
+++ To Do App/00_repetition.js
@@ -1,3 +1,9 @@
 /** Exercise – List all tasks 
  * Bonus – List only incomplete tasks
 */
+
+/** Exercise
+ * In index.html create a heading h1, h2 and h3. In all of them will be any text. For all headings, set
+ * that will change the text to "Click below". The result will be that if you click on h1, this will change
+ * the text and you need to click on heading h2 etc.
+ */
--- toDoApp.js
+++ toDoApp.js
@@ -8,3 +8,32 @@ document.querySelector('a').addEventListener('click', (event) => {
   console.log(event.target.textContent)
   event.target.textContent = 'It was clicked'
 })
+
+
+/** Exercise
+ * In index.html create a heading h1, h2 and h3. In all of them will be any text. For all headings, set
+ * that will change the text to "Click below". The result will be that if you click on h1, this will change
+ * the text and you need to click on heading h2 etc.
+ */
+
+// document.querySelector('h1').addEventListener('click', (event) => {
+//   console.log(event.target.textContent)
+//   event.target.textContent = 'Click below'
+// })
+
+// document.querySelector('h2').addEventListener('click', (event) => {
+//   console.log(event.target.textContent)
+//   event.target.textContent = 'Click below'
+// })
+
+// document.querySelector('h3').addEventListener('click', (event) => {
+//   console.log(event.target.textContent)
+//   event.target.textContent = 'Click below'
+// })
+
+document.querySelectorAll('h1, h2, h3').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    console.log(event.target.textContent)
+    event.target.textContent = 'Click below'
+  })
+})

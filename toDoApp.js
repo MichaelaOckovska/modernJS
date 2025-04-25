099e1942112af0d9e984daec5b3818d3911aaa83
Michaela Očkovská <michaela.ockovska@hptronic.cz>
2025-04-25 06:28:12 +0200

    JS78.06

--- toDoApp.js
+++ toDoApp.js
@@ -30,27 +30,6 @@ paragraph.textContent = `Remaining tasks: ${tasksLeft.length}`
 
 document.querySelector('main').appendChild(paragraph)
 
-
-/** Exercise – List all tasks 
-* Bonus – List only incomplete tasks
-*/
-
-const firstSubtitle = document.createElement('h3')
-firstSubtitle.textContent = `My solution`
-document.querySelector('main').append(firstSubtitle)
-
-myToDos.forEach(task => {
-  const taskEl = document.createElement('p')
-  taskEl.textContent = task.text
-  document.querySelector('main').appendChild(taskEl)
-})
-
-/** Solution from video */
-
-const secondSubtitle = document.createElement('h3')
-secondSubtitle.textContent = `Solution from video`
-document.querySelector('main').append(secondSubtitle)
-
 for (let i = 0; i < 4; i++) { } // Pamätaj, JS počíta od nuly
 
 for (let i = 0; i < myToDos.length; i++) {

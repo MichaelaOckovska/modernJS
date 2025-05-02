78a32acbb7488e5ddcf12d7f4e60223d2668e176
Michaela Očkovská <michaela.ockovska@hptronic.cz>
2025-05-02 06:24:51 +0200

    JS85.01

---
+++ To Do App/04.form.js
@@ -0,0 +1,20 @@
+/** Working with FORM */
+
+document.querySelector('#test-form').addEventListener('submit', (event) => {
+    // Vypnúť východzie nastavenie formulára
+    event.preventDefault()
+
+    // Prístup k obsahu inputu
+    const inputContent = event.target.elements.firstName.value
+    console.log(inputContent)
+
+    // Vytvoriť odstavec a pridáme do nej text z inputu
+    const paragraph = document.createElement('p')
+    paragraph.textContent = inputContent
+
+    document.querySelector('#form-output').appendChild(paragraph)
+
+    // Po odoslaní vymazať obsah inputu
+    event.target.elements.firstName.value = ""
+})
+

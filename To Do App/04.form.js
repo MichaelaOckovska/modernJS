3334894502e4fc4117db3007f55b647f6393818c
Michaela Očkovská <michaela.ockovska@hptronic.cz>
2025-05-04 21:01:37 +0200

    a

--- To Do App/04.form.js
+++ To Do App/04.form.js
@@ -18,6 +18,7 @@ document.querySelector('#test-form').addEventListener('submit', (event) => {
     event.target.elements.firstName.value = ""
 })
 
+
 /** Exercise
  * Create any three fields – first name, last name, email
  * Press send – the fields are emptied and the data is written down in paragraphs

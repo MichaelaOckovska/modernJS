ab3819c66fa9817f896720dfad732247d19633d3
MichaelaOckovska <michaela.ockovska@gmail.com>
2025-05-25 12:08:57 +0200

    Fix

--- To Do App/05_changingCSS.js
+++ To Do App/05_changingCSS.js
@@ -42,3 +42,27 @@ document.querySelector('body').addEventListener('keydown', (e) => { // Už som t
         square.style.top = `${newTop}px`
     }
 })
+
+
+/** GetComputedStyle a vytiahnutie vlastností CSS */
+
+const heading = document.querySelector('h1')
+
+const style = getComputedStyle(heading)
+console.log(style.color) // Vypíše v rgb notácii
+console.log(style.backgroundColor)
+
+
+/** Exercise – Enter a color in the box and change the background */
+
+const colorForm = document.querySelector('#color-form')
+let myBody = document.querySelector('body')
+
+colorForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let inputColor = e.target.color.value
+    
+    myBody.style.backgroundColor = inputColor
+    e.target.reset()
+})
+

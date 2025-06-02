3af1c670c6b1d1a6b6e065e1fc5c92a56013695e
MichaelaOckovska <michaela.ockovska@gmail.com>
2025-06-02 22:27:38 +0200

    scrolling

---
+++ To Do App/06_scrolling.js
@@ -0,0 +1,22 @@
+/** Scrollovanie */
+
+window.addEventListener('scroll', (e) => {
+    const scrolled = window.scrollY
+    const documentLength = document.documentElement.scrollHeight
+    const innerHeight = window.innerHeight   // Obsah stránky/okna, kde neskrolujeme (to čo vidíme hneď po otvorení okna)
+    const scrollable = documentLength - innerHeight
+
+    // console.log(scrollable) // Maximálna dĺžka skrolovania
+    // console.log(scrolled)   // Aktuálna dĺžka skrolovania
+
+    if (scrolled === scrollable) {
+        alert('You have reached the bottom of the page!')
+    }
+
+    // if (Math.ceil(scrolled) === scrollable) { // Ak by som potrebovala zaokrúhliť na celé čísla
+    //     alert('You have reached the bottom of the page!')
+    // }
+
+    /** Samozrejme, nemusíme dôjsť až na koniec. Týmto spôsobom môžeme nastaviť "urob niečo" v ľubovoľnom momente počas skrolovania. */
+})
+
--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-/** Scrollovanie */
-
+/** Ikona "Nahor" */
 window.addEventListener('scroll', (e) => {
     const scrolled = window.scrollY
-    const documentLength = document.documentElement.scrollHeight
-    const innerHeight = window.innerHeight   // Obsah stránky/okna, kde neskrolujeme (to čo vidíme hneď po otvorení okna)
-    const scrollable = documentLength - innerHeight
-    
-    // console.log(scrollable) // Maximálna dĺžka skrolovania
-    // console.log(scrolled)   // Aktuálna dĺžka skrolovania
+    const documentLength = document.documentElement.scrollHeight // Celková dĺžka dokumentu
+    const scrollable = documentLength - window.innerHeight
 
-    if (scrolled === scrollable) {
-        alert('You have reached the bottom of the page!')
+    if (scrolled === scrollable) { // Ak by mi to nefungovalo, skúsiť zaokrúhliť
+        const upBtn = document.querySelector('.up-btn')
+        upBtn.style.display = "block"
     }
-    
-    // if (Math.ceil(scrolled) === scrollable) { // Ak by som potrebovala zaokrúhliť na celé čísla
-    //     alert('You have reached the bottom of the page!')
-    // }
-
-    /** Samozrejme, nemusíme dôjsť až na koniec. Týmto spôsobom môžeme nastaviť "urob niečo" v ľubovoľnom momente počas skrolovania. */
 })
-

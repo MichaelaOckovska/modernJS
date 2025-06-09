c99b4c4d55319ad6f86a51c1034d8a6afef97134
MichaelaOckovska <michaela.ockovska@gmail.com>
2025-06-09 06:08:38 +0200

    JS96 - fin

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/** Ikona "Nahor" */
+/** Button "Nahor" */
+
 window.addEventListener('scroll', (e) => {
     const scrolled = window.scrollY
     const documentLength = document.documentElement.scrollHeight // Celková dĺžka dokumentu
@@ -7,5 +8,21 @@ window.addEventListener('scroll', (e) => {
     if (scrolled === scrollable) { // Ak by mi to nefungovalo, skúsiť zaokrúhliť
         const upBtn = document.querySelector('.up-btn')
         upBtn.style.display = "block"
+
+        upBtn.addEventListener('click', (e) => {
+            upBtn.style.display = "none"
+        })
     }
 })
+
+
+/** Plynulé skrolovanie a scrollTo */
+
+window.scrollTo(0, 300) // Potrebujem súradnice na osiach x a y, tento zápis ma posunie nižšie po osy y
+
+window.scrollTo({ // Môžem zapísať aj objekt
+    top: 300,
+    left: 100,
+    behavior: 'smooth'
+}) 
+
